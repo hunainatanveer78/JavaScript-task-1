a563e0fc7574bf1f17870110aa5b04c7aedd9ae2
WEB DEVELOPEMENT with Hunaina <161623039+hunainatanveer78@users.noreply.github.com>
2024-06-06 13:18:46 +0500

    Hunaina developer
    Hunaina developer make task 1 of Final test

---
+++ app.js
@@ -0,0 +1,12 @@
+a = 'a is 1 <br> <br>  ' 
+document.write(a);
+b = 'b is 2 <br> <br>  '
+document.write(b);
+value = 'result is <br>  <br> '
+
+document.write(value);
+
+var a = 1 
+var b = 2
+var value = 
+document.write(a+b);
